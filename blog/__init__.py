9196ef3bbc94bdfc2443643ea0822757f20ecdb2
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-10-20 10:48:09 +0200

    check pep8

--- blog/__init__.py
+++ blog/__init__.py
@@ -8,11 +8,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from blog import routes, models
+from blog import models
+
 
 @app.shell_context_processor
 def make_shell_context():
-  return {
-      "db": db,
-      "Entry": models.Entry
-  }
+    return {
+        "db": db,
+        "Entry": models.Entry
+    }
