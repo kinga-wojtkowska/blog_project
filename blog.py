1a8228b2d9717fac92f5af3d014755ffad50a0d9
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-10-20 10:52:27 +0200

    check pep8

--- blog.py
+++ blog.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
 from blog.models import Entry, db
 from faker import Faker
 
+
 app = Flask(__name__)
+
 
 def create_posts(classid, number):
     fake = Faker()
     for x in range(number+1):
         if classid == Entry:
-            post = Entry(title=fake.sentence(), body=fake.paragraph(10),is_published = True)
+            post = Entry(title=fake.sentence(), body=fake.paragraph(10), is_published=True)  # noqa: E501
             db.session.add(post)
         db.session.commit()
+
 
 create_posts(Entry, 10)
